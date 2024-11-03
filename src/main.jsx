08a5d8cce23ce0752e090a50108cb1492b16ac19
torikallcode <torikalcontent@gmail.com>
2024-11-03 15:08:04 +0800

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import { Home } from './pages/home'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ src/pages/home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est libero quasi harum impedit quam unde reprehenderit, quaerat distinctio, quo itaque, facere numquam nisi id ducimus! Commodi odio iure repellat iusto error dolorum quae minima! Unde quod doloremque consequuntur, dolore totam saepe praesentium esse blanditiis, fugit, dolores laborum? Possimus repellat consequatur veniam assumenda, beatae nemo officia dolore. Consequuntur cum quam ducimus ex consectetur, esse eveniet, eligendi porro possimus delectus similique obcaecati. Maxime obcaecati, voluptatem tempora modi dolore omnis mollitia quas culpa magnam non quisquam vitae fugiat officiis dolores dolorem eaque voluptate aperiam cum vel perferendis inventore labore! Reiciendis officiis alias quidem.</div>
+  )
+}
